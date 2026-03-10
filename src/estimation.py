3c5fbe7ede4198b373d4e063fa816cbe2c882124
ADIL MUHAMMED Y <adilmuhammedy@gmail.com>
2026-03-10 21:54:47 +0530

    change respone commit

--- src/estimation.py
+++ src/estimation.py
@@ -5,12 +5,13 @@ from schemas import BodyResponse
 model = joblib.load("../models/body_model2.pkl")
 
 
+
 def predict_body(height: float, weight: float, age:int, gender: int) -> BodyResponse:
     input_data = np.array([[height, weight, age, gender]])
     prediction = model.predict(input_data)
 
     return BodyResponse(
-        chest=float(prediction[0][0]),
-        waist=float(prediction[0][1]),
-        shoulder=float(prediction[0][2])
-    )
+     chest=round(float(prediction[0][0]), 2),
+     waist=round(float(prediction[0][1]), 2),
+     shoulder=round(float(prediction[0][2]), 2)
+)
